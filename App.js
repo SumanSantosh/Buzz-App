b71aefecb082526727b0308099d1429547e6efbe
Prasanna Kumar K <prasanna.kotha@hexagon.com>
2021-05-05 19:58:58 +0530

    Displayed Tow tabs

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import FbScreen from "./screens/fb"
+import InstaScreen from "./screens/in"
+import {createBottomTabNavigator} from "react-navigation-tabs"
+import {createAppContainer} from "react-navigation"
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppContainer/>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const TabNavigator = createBottomTabNavigator({
+  Facebook:{screen:FbScreen},
+  Instagram:{screen:InstaScreen}
+})
+
+const AppContainer = createAppContainer(TabNavigator)
